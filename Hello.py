713c3217f7c25b005d6fe0eda80044d6fb245527
Akshat <akshatsharma0610@gmail.com>
2023-04-11 22:49:50 +0530

    Commit2

--- Hello.py
+++ Hello.py
@@ -1,4 +1,12 @@
 import streamlit as st;
 st.set_page_config(page_title="Forbes List Analysis");
 st.write("# Welcome 👋")
-st.markdown("Forbes List Analysis")
+st.write("# Forbes List Analysis")
+st.markdown("Forbes is well-known for its lists, which it uses to rank people and companies in a variety of categories. Some of its most popular lists include: ")
+st.markdown("- The Billionaires List")
+st.markdown("- The World's Most Powerful People")
+st.markdown("- 30 Under 30")
+st.markdown("- The World's Highest-Paid Athletes")
+st.markdown("- The Celebrity 100: The World's Highest-Paid Entertainers")
+st.markdown("- The Global 2000: The World's Largest Public Companies")
+st.markdown("- America's Top Colleges")
